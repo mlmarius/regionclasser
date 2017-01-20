40b07807576b819d503dfebdc1039b221a7803da
Liviu Marius Manea <mlmarius@yahoo.com>
2017-01-20 13:00:47 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 setup(name='regionclasser',
-            version='1.0',
-            description="Classify a point into different region classes with respect to Romania.",
-            author="Liviu Manea",
-            url="https://github.com/mlmarius/regionclasser",
-            license="MIT",
-            packages=['regionclasser'],
-            install_requires=['fiona', 'shapely'],
-            package_data={'regionclasser': ['shapefiles/*']},
-            zip_safe=False
-            )
+      version='1.0',
+      description="Classify a point into different region classes with respect to Romania.",
+      author="Liviu Manea",
+      url="https://github.com/mlmarius/regionclasser",
+      license="MIT",
+      packages=['regionclasser'],
+      install_requires=['fiona', 'shapely'],
+      package_data={'regionclasser': ['shapefiles/*']},
+      zip_safe=False
+)
